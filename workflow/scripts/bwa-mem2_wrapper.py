344fc92f6a46d10b26d38692391adf4abb0426c0
sroener <40714954+sroener@users.noreply.github.com>
2022-02-22 17:43:54 +0100

    initial commit

---
+++ workflow/scripts/bwa-mem2_wrapper.py
@@ -0,0 +1,12 @@
+# Any Python script in the scripts folder will be able to import from this module.
+__author__ = "Sebastian Röner"
+__copyright__ = (
+    "Copyright 2022, Sebastian Röner"
+)
+__email__ = "sebastian.roener@bih-charite.de"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
